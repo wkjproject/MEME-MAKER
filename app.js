ffe375224875a2797efb0c530744fdd417e318bf
jwk <nakazjk@naver.com>
2023-08-18 10:01:41 +0900

    meme maker

---
+++ app.js
@@ -0,0 +1,135 @@
+const fileInput = document.querySelector("#file");
+const saveBtn = document.querySelector("#save");
+const textInput = document.querySelector("#text");
+const modeBtn = document.querySelector("#mode-btn");
+const destroyBtn = document.querySelector("#destroy-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+const color = document.querySelector("#color");
+const colorOptions = Array.from(document.querySelectorAll(".color-option"));
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const lineWidth = document.querySelector("#line-width");
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.linecap = "round";
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if(isPainting){
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting(event){
+  isPainting = true;
+
+}
+function cancelPainting(event){
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event){
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event){
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+
+}
+
+function onColorClick(event){
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onModeClick() {
+  if(isFilling){
+    isFilling = false;
+    modeBtn.innerHTML = "<strong>🖌️Draw</strong> or 🧺Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerHTML = "🖌️Draw or <strong>🧺Fill</strong>";
+
+  }
+}
+
+function onCanvasClick(){
+  if(isFilling){
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick(){
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick(){
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
+
+function onFileChange(event){
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function(){
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  }
+}
+
+function onDoubleClick(event){
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "68px serif";
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+
+function onSaveClick(){
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+
+
+
+canvas.addEventListener("dblclick", onDoubleClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave",cancelPainting);
+canvas.addEventListener("click",onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click",onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
